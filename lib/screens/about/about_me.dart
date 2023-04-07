a619d48ae2fee47116c9c114f67b55c009eba66e
Jayeshgiri Bavaji <jbavaji@gmail.com>
2023-04-07 17:30:38 +0530

    add: about me changes

--- lib/screens/about/about_me.dart
+++ lib/screens/about/about_me.dart
@@ -1,38 +1,25 @@
-import 'package:app/screens/skills/technology_stack.dart';
-import 'package:app/screens/portfolio/app_item.dart';
+import 'package:app/screens/about/education_info.dart';
+import 'package:app/screens/about/personal_info.dart';
 import 'package:flutter/material.dart';
 
-import 'info_item.dart';
-
-class AboutMe extends StatefulWidget {
+class AboutMe extends StatelessWidget {
   const AboutMe({Key? key}) : super(key: key);
-
-  @override
-  State<AboutMe> createState() => _AboutMeState();
-}
-
-class _AboutMeState extends State<AboutMe> {
-  @override
-  void initState() {
-    setState(() {});
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              // Instead of two different colors here I want to have the two other Linear gradients
-              // with each having two other different colors that go from top to bottom
-              Colors.green.withOpacity(0.15),
-              Colors.transparent,
-            ],
-            stops: const [0.8, 0.2],
-            tileMode: TileMode.clamp,
-          ),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            // Instead of two different colors here I want to have the two other Linear gradients
+            // with each having two other different colors that go from top to bottom
+            Colors.green.withOpacity(0.15),
+            Colors.transparent,
+          ],
+          stops: const [0.8, 0.2],
+          tileMode: TileMode.clamp,
         ),
+      ),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -41,9 +28,11 @@ class _AboutMeState extends State<AboutMe> {
             padding: const EdgeInsets.symmetric(horizontal: 30),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
               children: const [
-                Expanded(child: InfoItem()),
-                // Expanded(flex: 2, child: TechnologyStack()),
+                PersonalInfo(),
+                Expanded(child: EducationInfo()),
               ],
             ),
           ),
--- lib/screens/about/personal_info.dart
+++ lib/screens/about/personal_info.dart
@@ -1,53 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class PersonalInfo extends StatelessWidget {
   const PersonalInfo({Key? key}) : super(key: key);
+
+  Widget infoWidget(BuildContext context, String text) {
+    return Text(
+      text,
+      style: Theme.of(context).textTheme.displayMedium?.copyWith(
+            fontWeight: FontWeight.w500,
+            color: Colors.grey.shade900,
+          ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * 0.50,
-      child:   Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Name: Jayeshgiri R. Bavaji',
-          style: Theme.of(context).textTheme.displayMedium,
-        ),
-        Text(
-          'Phone: (+91) 96018 33544',
-          style: Theme.of(context).textTheme.displayMedium,
-        ),
-        Text(
-          'Email: jbavaji@gmail.com',
-          style: Theme.of(context).textTheme.displayMedium,
-        ),
-        Text(
-          'Address: 303, Sai Miracle, Pune',
-          style: Theme.of(context).textTheme.displayMedium,
-        ),
-        Text(
-          'Country: India',
-          style: Theme.of(context).textTheme.displayMedium,
-        ),
-        Text(
-          'Languages: English, Hindi, Gujrati',
-          style: Theme.of(context).textTheme.displayMedium,
-        ),
-        Text(
-          'FreeLance: Available',
-          style: Theme.of(context).textTheme.displayMedium,
-        ),
-        ElevatedButton.icon(
-          onPressed: () {
-
-          },
-          icon: const FaIcon(FontAwesomeIcons.download, size: 20,),
-          label: const Text('Download CV'),
-        ),
-      ],
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Personal Information',
+            style: Theme.of(context)
+                .textTheme
+                .displayLarge
+                ?.copyWith(fontWeight: FontWeight.w500, fontSize: 42),
+          ),
+          infoWidget(
+            context,
+            'Name: Jayeshgiri R. Bavaji',
+          ),
+          infoWidget(
+            context,
+            'Phone: (+91) 96018 33544',
+          ),
+          infoWidget(
+            context,
+            'Address: 303, Sai Miracle, Pune',
+          ),
+          infoWidget(
+            context,
+            'Country: India',
+          ),
+          infoWidget(
+            context,
+            'Languages: English, Hindi, Gujrati',
+          ),
+          infoWidget(
+            context,
+            'FreeLance: Available',
+          ),
+          // ElevatedButton.icon(
+          //   onPressed: () {},
+          //   icon: const FaIcon(
+          //     FontAwesomeIcons.download,
+          //     size: 20,
+          //   ),
+          //   label: const Text('Download CV'),
+          // ),
+        ],
       ),
     );
   }
